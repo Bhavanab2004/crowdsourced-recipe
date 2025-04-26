5ad36f6dae3f3d0e71151fdb3688d8c80d6832f7
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2025-04-27 00:05:13 +0530

    Update CreateRecipe.jsx

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -35,7 +35,7 @@ export default function CreateRecipe() {
   };
 
   const createRecipe = async (recipeData) => {
-    await axios.post("http://your-api-endpoint.com/recipes", recipeData);
+    await axios.post("https://crowdsourced-recipe.onrender.com/.com/recipes", recipeData);
   };
 
   const handleSubmit = async (e) => {
